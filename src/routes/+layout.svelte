a64b19a3035b4a1f4f16a7270a4b32ee43afc7ae
je-boska <joneirikboska@gmail.com>
2023-08-07 11:36:43 +0200

    Add page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,20 @@
 <script>
 	import '../app.css';
+
+	import { fly } from 'svelte/transition';
+	import Nav from '../components/Nav.svelte';
+
+	export let data;
 </script>
 
 <div class="app">
+	<Nav />
 	<main>
-		<slot />
+		{#key data.url}
+			<div in:fly={{ x: 300, duration: 300, delay: 300 }} out:fly={{ x: -300, duration: 300 }}>
+				<slot />
+			</div>
+		{/key}
 	</main>
 </div>
 
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,24 @@
+<section>
+	<h1>This is a test</h1>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam temporibus odit odio
+		perspiciatis modi at pariatur natus aliquid praesentium dolores, quasi quod voluptatem quo,
+		corporis adipisci voluptates iusto nesciunt amet dolorem aliquam. Excepturi voluptate dolorum
+		quaerat odit error in voluptates est aliquam molestiae, sint harum quod voluptatibus eligendi
+		nulla veniam accusantium amet unde labore nisi natus totam ea rem. Minima veniam maiores ex
+		recusandae corrupti esse facere, laboriosam labore itaque laborum adipisci numquam vel eaque
+		exercitationem beatae quibusdam? Minus sequi reiciendis eligendi molestias. Explicabo earum,
+		voluptates sit, excepturi maiores iure facere magnam numquam odio, magni similique quae
+		doloribus quam sed! Odio quod numquam est ratione quidem ea eos reiciendis dolor sunt? Ea vero
+		perferendis veniam est atque dolorum itaque delectus natus libero ab minima dolorem, velit odio
+		hic quidem recusandae fugit eius, dicta ipsa! Veniam ipsa alias obcaecati cum ea, nulla a quam
+		illum, possimus aut distinctio totam ipsum cupiditate reprehenderit quos. Cupiditate accusantium
+		deserunt praesentium iusto quibusdam blanditiis nam maxime rem placeat architecto repellat magni
+		quas et, sit possimus voluptate corporis velit, tenetur nisi eaque beatae repellendus molestias
+		vero laboriosam. Unde optio incidunt ex iure. Veniam quo tempora maxime voluptatum deserunt, id
+		sit molestiae labore voluptatibus non ipsa eos dolore omnis quaerat repellendus delectus sequi
+		at fugiat necessitatibus reprehenderit earum nulla minima ea aperiam. Neque consectetur, modi,
+		hic et accusantium, aliquam ipsum tempore incidunt asperiores unde nihil culpa doloremque alias
+		eligendi minima numquam deserunt porro. At soluta consequuntur dolorum.
+	</p>
+</section>
